6c93d9d94dbc5a178433c39eeb7b428e49b4ed31
Rodrigo de Castro Cardoso <74211714+Rodrigocastrocard@users.noreply.github.com>
2022-09-28 22:15:47 -0300

    Add files via upload

--- testes.py
+++ testes.py
@@ -4,46 +4,13 @@ from mysql.connector import Error
 from datetime import datetime
 from tkinter import*
 
-
-
 con = mysql.connector.connect(host='localhost', database='templarios', user='root', password='Janete4353')
 
-tela = Tk()
-tela.title('Cadastro de Entidade')
-tela.state("zoomed")
+def cadastros():
+    tela.destroy()
+    import tela_menu_cadastro
 
-
-imagem = PhotoImage(file="images\\cadent.png")
-
-lab_imagem=Label(tela, image=imagem)
-lab_imagem.pack()
-
-#nomemembro = input('Digite o nome. ')
-
-nomemembro = Entry(tela, bd=2, font=("Calibri",15),justify=CENTER)
-nomemembro.place(width=500, height=30, x=400, y=50)
-
-telefone = Entry(tela, bd=2, font=("Calibri",15),justify=CENTER)
-telefone.place(width=200, height=30, x=400, y=120)
-
-endereco = Entry(tela, bd=2, font=("Calibri",15),justify=CENTER)
-endereco.place(width=600, height=30, x=400, y=190)
-
-nascimento = Entry(tela, bd=2, font=("Calibri",15),justify=CENTER)
-nascimento.place(width=150, height=30, x=400, y=260)
-
-candidatura = Entry(tela, bd=2, font=("Calibri",15),justify=CENTER)
-candidatura.place(width=150, height=30, x=400, y=330)
-
-emergencia = Entry(tela, bd=2, font=("Calibri",15),justify=CENTER)
-emergencia.place(width=200, height=30, x=400, y=390)
-
-sangue = Entry(tela, bd=2, font=("Calibri",15),justify=CENTER)
-sangue.place(width=50, height=30, x=400, y=460)
-
-tela.mainloop()
-
-
+#codigo comentado,pois essas informações serão informadas nas caixas das telas.
 '''
 
 telmembro = input('Digite o número do Telefone Celular: ')
@@ -142,6 +109,91 @@ cursor.close()
 
 '''
 
+
+
+
+
+def salvar():
+    inserir = """INSERT INTO membros
+    							( nomemembro, telmembro, enderecomembro, nascimentomembro, candidaturamembro, emergenciamembro, sanguemembro, data_cadastro) 
+    							values (%s, %s, %s, %s, %s, %s, %s,%s);						
+    							"""
+    sql_data = (
+        nomemembro, telFormatado, enderecomembro, data_formatada, data_formatadacand, telemerFormatado, sanguemembro,
+        data_cadastro)
+
+    cursor = con.cursor()
+    cursor.execute(inserir, sql_data)
+    con.commit()
+    print(cursor.rowcount, 'Registro inserido com sucesso.\n')
+    cursor.close()
+
+
+
+tela = Tk()
+tela.title('Cadastro de Entidade')
+tela.state("zoomed")
+
+
+imagem = PhotoImage(file="images\\cadent.png")
+
+lab_imagem=Label(tela, image=imagem)
+lab_imagem.pack()
+
+
+#criando variaveis
+
+
+
+
+
+
+#nomemembro = input('Digite o nome. ')
+
+#quando inserir informações dentro das caixas, gravar nas variaveis para salvar posteriormente
+# (assim como está comentado )
+
+
+
+nomemembro = Entry(tela, bd=2, font=("Calibri",15),justify=CENTER)  #gravar informação na variavel para salvar
+nomemembro.place(width=500, height=30, x=400, y=50)
+
+telefone = Entry(tela, bd=2, font=("Calibri",15),justify=CENTER)  #gravar informação na variavel para salvar
+telefone.place(width=200, height=30, x=400, y=120)
+
+endereco = Entry(tela, bd=2, font=("Calibri",15),justify=CENTER) #gravar informação na variavel para salvar
+endereco.place(width=600, height=30, x=400, y=190)
+
+nascimento = Entry(tela, bd=2, font=("Calibri",15),justify=CENTER) #gravar informação na variavel para salvar
+nascimento.place(width=150, height=30, x=400, y=260)
+
+candidatura = Entry(tela, bd=2, font=("Calibri",15),justify=CENTER) #gravar informação na variavel para salvar
+candidatura.place(width=150, height=30, x=400, y=330)
+
+emergencia = Entry(tela, bd=2, font=("Calibri",15),justify=CENTER) #gravar informação na variavel para salvar
+emergencia.place(width=200, height=30, x=400, y=390)
+
+sangue = Entry(tela, bd=2, font=("Calibri",15),justify=CENTER) #gravar informação na variavel para salvar
+sangue.place(width=50, height=30, x=400, y=460)
+
+
+    #criacao de botões
+
+#Quando clicar em salvar, tem que gravar as informações digitadas nas caixas
+
+sair_botao = Button(tela, bd=0, text='SAIR', command=tela.destroy)
+sair_botao.place(width=87, height=51, x=1150, y=600)
+
+
+salvar_botao = Button(tela, bd=0, text='SALVAR', command=tela.destroy)
+salvar_botao.place(width=87, height=51, x=100, y=600)
+
+voltar_botao = Button(tela, bd=0, text='VOLTAR', command=cadastros)
+voltar_botao.place(width=87, height=51, x=200, y=600)
+
+
+
+tela.mainloop()
 
 
 
